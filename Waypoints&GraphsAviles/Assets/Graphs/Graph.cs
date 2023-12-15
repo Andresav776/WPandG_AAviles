2fbd2c6c3129f8a258d60453d26922e926897fc2
Andresav776 <715867@student.dusd.net>
2023-12-15 13:50:30 -0800

    Halfway through Waypoints & Graphs

---
+++ Waypoints&GraphsAviles/Assets/Graphs/Graph.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph
+{
+    List<Edge> edges = new List<Edge>();
+    List<Node> nodes = new List<Node>();
+    List<Node> pathList = new List<Node>();
+
+    public Graph()
+    {
+
+    }
+}
